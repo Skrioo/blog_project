a6f3af7c9bc2fb46a95d4c911ccc052c7e003cfe
Skrioo <skrioo@DESKTOP-FFA3647.localdomain>
2022-11-04 01:58:32 +0100

    added tokens

--- core/views.py
+++ core/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+from core.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-def front(request):
-    context = { }
-    return render(request, "index.html", context)
+# def front(request):
+#     context = { }
+#     return render(request, "index.html", context)
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/core/token/',
+        '/core/register/',
+        '/core/token/refresh/'
+    ]
+    return Response(routes)
 
